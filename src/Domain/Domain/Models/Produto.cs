6c2df2257cb775de4ce814c9108b409ce23b2f3a
William Nascimento <william.nascimento2010@gmail.com>
2021-02-04 10:42:03 -0300

    .gitignore

---
+++ src/Domain/Domain/Models/Produto.cs
@@ -0,0 +1,11 @@
+namespace Domain.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string  Nome { get; set; }
+        public double ValorVenda { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
